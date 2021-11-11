f7ff70085032cc00d1bec69c3367828e8b3b5ffb
Mateus <mateusmaranhao0210@gmail.com>
2021-11-11 10:47:18 -0300

    feat: adicionar nome cadastrado no array

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 window.addEventListener('load', start);
 
-var globalNames = ['Mateus', 'Filipe', 'Andréia', 'Adriano']
+var globalNames = []
 var inputName = null;
 
 function start() {
-  inputName = document.querySelector('#inputName');
-
   preventFormSubmit();
+
+  inputName = document.querySelector('#inputName');
   activateInput();
 }
 
 function preventFormSubmit() {
+  var form = document.querySelector('form');
+  form.addEventListener('submit', handleFormSubmit);
+  
   function handleFormSubmit(event) {
     event.preventDefault();
   }
-  
-  var form = document.querySelector('form');
-  form.addEventListener('submit', handleFormSubmit);
 }
 
 function activateInput() {
+  inputName.addEventListener('keyup', handleTyping);
+
+  function handleTyping(event) {
+    if(event.key === 'Enter') {
+      insertName(event.target.value)
+    }
+  }
+
+  function insertName(newName) {
+    globalNames.push(newName);
+
+    console.log(globalNames)
+  }
+
   inputName.focus();
 };
